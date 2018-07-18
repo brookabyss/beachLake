35f5ef30ee180649a536c960a7139bf6de5bd6f6
Brook Kebede <brookabyss@gmail.com>
2018-07-18 16:21:45 -0700

    Initial Commit

---
+++ beachLake/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using beachLake.Models;
+
+namespace beachLake.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Beach()
+        {
+            ViewData["Message"] = "Picture of a beach.";
+
+            return View();
+        }
+
+        public IActionResult Lake()
+        {
+            ViewData["Message"] = "Picture of a Lake";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
